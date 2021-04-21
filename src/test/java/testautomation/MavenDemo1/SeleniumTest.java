b4f1b138be8cb7831b00c4454ff13ab8d49fb608
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-21 18:20:02 +0530

    Local commit

---
+++ src/test/java/testautomation/MavenDemo1/SeleniumTest.java
@@ -0,0 +1,19 @@
+package testautomation.MavenDemo1;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void InvokeBrowser()
+	{
+		System.out.println("Invoking browser");
+	}
+	
+	@Test
+	public void ValidateElements()
+	{
+		System.out.println("Validate WebElements");
+	}
+
+}
